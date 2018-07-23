b7a5ca75aaf7d28c2b1997e528a4684030fe9c60
Shailesh Rudra <rudra12shailesh@gmail.com>
2018-07-23 23:10:26 +0530

    class styling with exapmles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,47 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // templateUrl: './app.component.html',
+  template: `
+  <h1> Rudra! </h1>
+  <p [class]="titleClass">Shailesh</p>
+  <p [class.pink-title]="titleClassFalse">Shailesh</p>
+  <p [class.yellow-title]="titleClass">Shailesh</p>
+  <p [ngClass]="titleClasses">Shailesh Rudra</p>
+
+  `,
+  styles: [`
+  h1 {
+    text-declaration: uderline;
+    color: red;
+  }
+
+  .green-title {
+    color: green;
+}
+
+  .yellow-title {
+    color: yellow;
+  }
+
+  .pink-title {
+    color: pink;
+  }
+
+  .large-title {
+    font-size: 4em;
+  }
+  `]
 })
 export class AppComponent {
   title = 'app';
+
+  titleClass = "green-title";
+  titleClassFalse = true;
+  // titleClassFalse = false;
+
+  titleClasses = {
+    'red-title': true,
+    'large-title': true
+  }
 }
